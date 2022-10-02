95e4884d1bbe7d28cf2d26a4f1cee76c860d5517
sbo <sbo@intractiv.com>
2022-10-02 14:40:14 +0200

    rename django_tuto_site app starships_site

--- django_tuto_root/starships_site/asgi.py
+++ django_tuto_root/starships_site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_tuto_site project.
+ASGI config for starships_site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_tuto_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'starships_site.settings')
 
 application = get_asgi_application()
--- django_tuto_root/starships_site/wsgi.py
+++ django_tuto_root/starships_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_tuto_site project.
+WSGI config for starships_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_tuto_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'starships_site.settings')
 
 application = get_wsgi_application()
