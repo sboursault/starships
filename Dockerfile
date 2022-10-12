555c389b02e06a1539c29ce002c705fd91bc954d
sbo <sbo@intractiv.com>
2022-10-12 20:58:51 +0200

    with postgres !! \o/

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY starships_root /code/
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["./start"]
--- starships_root/starships_site/settings.py
+++ starships_root/starships_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,13 +77,14 @@ WSGI_APPLICATION = 'starships_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'OPTIONS': {
-        #    'service': 'my_service',
-        #    'passfile': '.my_pgpass',
-        #},
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME', 'starships'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
